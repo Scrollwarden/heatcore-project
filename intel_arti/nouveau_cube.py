efc18cb052700e5a1f075f4fdb97c31f805766dd
louschanen <louschanen@gmail.com>
2024-09-28 16:58:48 +0200

    Création du décorateur check_type et amélioration de l'affichage du cube
    L'affichage se fait désormais sous la forme du patron du Cube

--- intel_arti/nouveau_cube.py
+++ intel_arti/nouveau_cube.py
@@ -4,30 +4,73 @@ from copy import deepcopy
 # La face à tourner par numéro d'action
 CORRESP_FACE_ACT = (1, 3, 4, 2, 0, 5)
 
+def check_type(ignore_first, *args_type : type) :
+    """Vérifie que tous les arguments d'une fonction soient du type demandé
+    
+    Param 
+    -------
+        args_type (type) : les types attendus pour chaque argument
+    
+    """
+    def decorateur(fonction) :
+        def verification(*args) :
+            for i in range(ignore_first, len(args)) :
+                if not isinstance(args[i], args_type[i - ignore_first]) :
+                    raise TypeError(f"L'argument {i} est du type {type(args[i])} au lieu du type {args_type[i - ignore_first]}")
+            return fonction(*args)
+        return verification
+    return decorateur
 
 class Cube :
-    """La classe Cube enregistre l'état du cube à tout instant et permet de le modifier"""
+    """La classe Cube enregistre l'état du cube à tout instant et permet de le modifier.
+    Un 0 indique une case vide. Un 1 indique une croix. Un -1 indique un rond.
+    """
 
     def __init__(self) -> None:
         """Initialise le Cube. Dans son état initial, celui-ci est entièrement vide"""
         self.reset_cube()
-        self.faces[0, 1] = array([1, 2, 3])
-        self.faces[4, 1] = array([4, 5, 6])
-        self.faces[5, 1] = array([7, 8, 9])
-        self.faces[2, 1] = array([10, 11, 12])
+        # Pour les tests
+        self.faces[0, 1] = array([1, -1, 1])
+        self.faces[4, 1] = array([1, -1, -1])
+        self.faces[5, 1] = array([1, 0, 0])
+        self.faces[2, 1] = array([0, 1, -1])
     
     def __str__(self) -> str:
         """Retourne un string dès lors qu'on fait str(Cube) par exemple dans print(Cube)
 
-        Returns:
+        Return:
             str: Le string retourné représentant le cube
         """
-        return str(self.faces)
+        ligne_vide = " " * 9
+        affichage = ""
+        for i in range(3) :
+            ligne = (f"{self.faces[3, i, j]:>2.0f}" for j in range(3))
+            affichage += ligne_vide + " ".join(ligne) + "\n"
+        for i in range(3) :
+            ligne = ((f"{self.faces[f, i, j]:>2.0f}" for j in range(3)) for f in (2, 0, 4, 5))
+            affichage += "|".join(" ".join(morceau) for morceau in ligne) + "\n"
+        for i in range(3) :
+            ligne = (f"{self.faces[1, i, j]:>2.0f}" for j in range(3))
+            affichage += ligne_vide + " ".join(ligne) 
+            affichage += "\n" if i != 2 else ""
+        return affichage
     
+    @check_type(True, int)
+    def afficher_face(self, face : int) :
+        """Affiche la face demandée dans le terminal.
+        
+        Param
+        ------
+            face (int) : La face à afficher
+        """
+        for i in range(3) :
+            print(*self.faces[face, i])
+
     def reset_cube(self) -> None:
         """Remet le Cube à son état initial."""
         self.faces = zeros((6, 3, 3))
     
+    @check_type(True, ndarray)
     def set_faces(self, faces : ndarray) -> None:
         """Change entièrement l'état du Cube par celui passé en argument.
         Param
@@ -40,6 +83,7 @@ class Cube :
             raise ValueError(f"L'array faces doit avoir la forme (6, 3, 3), mais la forme actuelle est {faces.shape}")
         self.faces = faces
     
+    @check_type(True, int, int, int)
     def placer_symb(self, ligne : int, colonne : int, joueur : int) :
         """Place un symbole à la position demandée sur la face supérieure.
         
@@ -57,6 +101,7 @@ class Cube :
             raise ValueError(f"Le paramètre joueur ne peut être que 1 ou -1. Il est actuellement {joueur}")
         self.faces[0, ligne, colonne] = joueur
 
+    @check_type(True, int, int, int, int)
     def ajouter_symb(self, face : int, ligne : int, colonne : int, joueur : int) -> None:
         """Ajoute un sympbole à la position demandée même si cette position n'est pas sur la face supérieure.
         Dans ce cas il ne s'agit pas d'un coup légal. Cette fonction n'a pas vocation à être utilisée pendant
@@ -80,6 +125,7 @@ class Cube :
             raise ValueError(f"Le paramètre joueur ne peut être que 1 ou -1. Il est actuellement {joueur}")
         self.faces[face, ligne, colonne] = joueur
 
+    @check_type(True, int, int)
     def jouer(self, action : int, joueur : int) -> None:
         """Joue l'action passée en argument.
         Les actions 0 à 17 tournent les faces du cube.
@@ -102,6 +148,7 @@ class Cube :
         else :
             self.jouer_symbole(action-18, joueur)
     
+    @check_type(True, int, int)
     def jouer_symbole(self, action : int, joueur : int) -> None:
         """Gère la partie des actions consistant à placer un symbole.
         Place donc le symbole du joueur à la position référée par action.
@@ -114,6 +161,7 @@ class Cube :
         ligne, colonne = divmod(action, 3)
         self.placer_symb(ligne, colonne, joueur)
 
+    @check_type(True, int)
     def jouer_tourner(self, action : int) -> None:
         """Gère la partie des actions consistant à tourner une face du cube.
 
@@ -127,8 +175,9 @@ class Cube :
             action -= 9
         if action < 6 :
             self.tourner_face(CORRESP_FACE_ACT[action], reverse)
-        self.tourner_couronne()
+        #self.tourner_couronne()
     
+    @check_type(True, int, bool)
     def tourner_face(self, face : int, reverse : bool = False) -> None :
         """Tourne la face sur elle-même dans le sens horaire par défaut et dans le sens anti-horaire.
         Attention : cette méthode ne modifie rien d'autre que la face spécifiée. Cela implique que les symboles
@@ -150,6 +199,7 @@ class Cube :
         self.faces[face, 0, 1] = (gauche, droite)[reverse]
         self.faces[face, 2, 1] = (droite, gauche)[reverse]
 
+    @check_type(True, list, bool, int)
     def tourner_couronne(self, liste_faces : list[int], est_ligne : bool, num : int) -> None:
         """Tourne la couronne de nombres associée à un mouvement.
         
@@ -164,9 +214,17 @@ class Cube :
                 self.faces[liste_faces[0], num, :], self.faces[liste_faces[1], num, :], self.faces[liste_faces[2], num, :], self.faces[liste_faces[3], num, :] = self.faces[liste_faces[1], num, :], self.faces[liste_faces[2], num, :], self.faces[liste_faces[3], num, :], self.faces[liste_faces[0], num, :]
         else :
             pass
+    
+    @check_type(True, bool, bool)
+    def symetrie(self, horizontale : bool, verticale : bool) :
+        """Renvoie l'état du Cube avec la ou les symétries axiales demandées
+        
+        Params
+        -------
+            horizontale (bool) : Si la symétrie horizontale doit être appliquée
+            verticale (bool) : Si la symétrie verticale doit être appliquée
+        """
 
 if __name__ == "__main__" :
     cube = Cube()
-    print(cube)
-    cube.tourner_couronne((0, 4, 5, 2), True, 1)
     print(cube)
