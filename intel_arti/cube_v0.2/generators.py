54d8019e12511c5798f7cfef3c1da958562a8ffb
Scrollwarden <156528205+Scrollwarden@users.noreply.github.com>
2024-11-06 16:54:51 +0100

    [add] allowed multi-geenration of the same state
    Chaque état gagné peut être généré plusieurs fois (avec les autres cases en aléatoire).
    La syntaxe de méthode interne a été enlevée des méthodes win_on_line, win_on_column et win_on_diagonal au cas où on veut entrainer l'agent que sur un seul type de cas.

--- intel_arti/cube_v0.2/generators.py
+++ intel_arti/cube_v0.2/generators.py
@@ -11,85 +11,86 @@ class GeneratorWinState:
     Génère des situations où le jeu est gagné par l'agent.
 
     METHODES
-    - randomize()
     - generate_state()
     '''
     def __init__(self):
         self.cube = Cube()
-        self.randomize()
 
-    def randomize(self):
-        """
-        Remplit aléatoirement le cube, de sorte que l'agent ne prête aucune attention aux cases qui ne forment pas de victoire.
-        """
-        self.cube.aleatoire()
-        self._SaD_random_wins()
-
-    def generate_state(self):
+    def generate_state(self, times=1):
         """
         Génère un état où le jeu est gagné par l'agent.
         Cette methode est un générateur qui fonctionne avec yield.
+
+        INPUT
+        - times (int) : le nombre de fois où chaque situation sera générée
+        avec le reste des cases définies aléatoirement (1 par défaut)
         """
         for face in range(6):
-            for line_win in self._win_on_line(face):
+            for line_win in self._win_on_line(face, times):
                 yield line_win
-            for column_win in self._win_on_column(face):
+            for column_win in self._win_on_column(face, times):
                 yield column_win
-            for diagonal_win in self._win_on_diagonal(face):
+            for diagonal_win in self._win_on_diagonal(face, times):
                 yield diagonal_win
         # TODO : any importances having many yield instead of one at the end ?
 
-    def _SaD_random_wins(self):
-        """
-        Seek and Destroy random wins. Sert à enlever les victoires générées aléatoirement.
-        Pas sûr que la methode soit nécessaire.
-        """
-        pass
-
-    def _win_on_line(self, face):
+    def win_on_line(self, face, times=1):
         """
         Génère un état où le jeu est gagné par l'agent sur une ligne.
         Cette methode est un générateur qui fonctionne avec yield.
-        Cette methode est interne à GeneratorWinState et ne devrait pas être utilisée directement.
 
         INPUTS
         - face (int) : la face sur laquelle on veut générer une victoire
+        - times (int) : le nombre de fois où la même génération sera générée (par défaut 1)
         """
-        for line in range(3):
-            situation = deepcopy(self.cube)
-            situation.set_ligne(face, line, array([1, 1, 1]))
-            yield situation
+        for _ in range(times):
+            for line in range(3):
+                situation = deepcopy(self.cube)
+                situation.aleatoire()
+                situation.set_ligne(face, line, array([1, 1, 1]))
+                yield situation
 
-    def _win_on_column(self, face):
+    def win_on_column(self, face, times=1):
         """
         Génère un état où le jeu est gagné par l'agent sur une colonne.
         Cette methode est un générateur qui fonctionne avec yield.
-        Cette methode est interne à GeneratorWinState et ne devrait pas être utilisée directement.
 
         INPUTS
         - face (int) : la face sur laquelle on veut générer une victoire
+        - times (int) : le nombre de fois où la même génération sera générée (par défaut 1)
         """
-        for column in range(3):
-            situation = deepcopy(self.cube)
-            situation.set_ligne(face, column, array([1, 1, 1]))
-            yield situation
+        for _ in range(times):
+            for column in range(3):
+                situation = deepcopy(self.cube)
+                situation.aleatoire()
+                situation.set_ligne(face, column, array([1, 1, 1]))
+                yield situation
 
-    def _win_on_diagonal(self, face):
+    def win_on_diagonal(self, face, times=1):
         """
         Génère un état où le jeu est gagné par l'agent sur une diagonale.
         Cette methode est un générateur qui fonctionne avec yield.
-        Cette methode est interne à GeneratorWinState et ne devrait pas être utilisée directement.
 
         INPUTS
         - face (int) : la face sur laquelle on veut générer une victoire
+        - times (int) : le nombre de fois où la même génération sera générée (par défaut 1)
         """
-        situation = deepcopy(self.cube)
-        for line, case in ((0, 0), (1, 1), (2, 2)):
-            situation.set_pion((face, line, case), 1)
-        yield situation
-        for line, case in ((0, 2), (1, 1), (2, 0)):
-            situation.set_pion((face, line, case), 1)
-        yield situation
+        # PS : c'est pas beau ici, j'aime pas les deux yield en cas-par-cas
+
+        # première diagonale
+        for _ in range(times):
+            situation = deepcopy(self.cube)
+            situation.aleatoire()
+            for line, case in ((0, 0), (1, 1), (2, 2)):
+                situation.set_pion((face, line, case), 1)
+            yield situation
+        # on recommence pour la diagonale inversée
+        for _ in range(times):
+            situation = deepcopy(self.cube)
+            situation.aleatoire()
+            for line, case in ((0, 2), (1, 1), (2, 0)):
+                situation.set_pion((face, line, case), 1)
+            yield situation
 
 
 
